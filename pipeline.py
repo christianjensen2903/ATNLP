5013c1c55ad9bc032e564c01ba2c0b4bb9705e8c
christianjensen2903 <cmj290300@gmail.com>
2022-12-12 22:47:55 +0100

    Go back to old since doesn't matter for experiment

--- pipeline.py
+++ pipeline.py
@@ -26,10 +26,8 @@ def train_iteration(input_tensor, target_tensor, encoder, decoder, encoder_optim
     # Prepare the initial decoder input
     decoder_input = torch.tensor([[scan_dataset.SOS_token]], device=device)
 
-    if decoder.rnn_type == 'LSTM':
-        decoder_hidden = encoder_hidden[0]
-    else:
-        decoder_hidden = encoder_hidden
+
+    decoder_hidden = encoder_hidden
 
     use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
 
