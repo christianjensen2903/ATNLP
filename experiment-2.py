5181f3899a42ae03e6b7698434f6cc5c53f21ed9
crivoi-andrei <crivoi@healper.dk>
2022-12-12 17:51:57 +0100

    experiment-2 wandb log

--- experiment-2.py
+++ experiment-2.py
@@ -11,7 +11,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pickle
 
-log_wandb = True
+log_wandb = False
 
 input_lang = scan_dataset.Lang()
 output_lang = scan_dataset.Lang()
@@ -33,7 +33,7 @@ test_dataset = scan_dataset.ScanDataset(
 MAX_LENGTH = max(train_dataset.input_lang.max_length, train_dataset.output_lang.max_length)
 
 n_iter = 100000
-n_runs = 5
+n_runs = 1
 
 overall_best = {
     'HIDDEN_SIZE': 200,  # 25, 50, 100, 200, or 400
@@ -58,7 +58,7 @@ print(f'Using device: {device}')
 def run_overall_best():
     results = []
     # Train 5 times and average the results
-    for _ in range(n_runs):
+    for run in range(n_runs):
         encoder = models.EncoderRNN(train_dataset.input_lang.n_words, overall_best['HIDDEN_SIZE'], device,
                                     overall_best['N_LAYERS'], overall_best['RNN_TYPE'], overall_best['DROPOUT']).to(
             device)
@@ -67,9 +67,9 @@ def run_overall_best():
                                     overall_best['ATTENTION']).to(device)
 
         encoder, decoder = pipeline.train(train_dataset, encoder, decoder, n_iter, print_every=100, learning_rate=0.001,
-                                          device=device)
-        pickle.dump(encoder, open('overall_best_encoder_exp_2.sav', 'wb'))
-        pickle.dump(decoder, open('overall_best_decoder_exp_2.sav', 'wb'))
+                                          device=device, log_wandb=True)
+        pickle.dump(encoder, open(f'overall_best_encoder_exp_2_run_{run}.sav', 'wb'))
+        pickle.dump(decoder, open(f'overall_best_decoder_exp_2_run_{run}.sav', 'wb'))
         results.append(pipeline.evaluate(test_dataset, encoder, decoder, max_length=MAX_LENGTH, verbose=False))
 
     avg_accuracy = sum(results) / len(results)
@@ -91,7 +91,9 @@ def run_experiment_best():
                                     experiment_best['ATTENTION']).to(device)
 
         encoder, decoder = pipeline.train(train_dataset, encoder, decoder, n_iter, print_every=100, learning_rate=0.001,
-                                          device=device, log_wandb=log_wandb)
+                                          device=device, log_wandb=True)
+        pickle.dump(encoder, open('experiment_best_encoder_exp_2.sav', 'wb'))
+        pickle.dump(decoder, open('experiment_best_decoder_exp_2.sav', 'wb'))
         results.append(pipeline.evaluate(test_dataset, encoder, decoder, max_length=MAX_LENGTH, verbose=False))
 
     avg_accuracy = sum(results) / len(results)
@@ -100,32 +102,12 @@ def run_experiment_best():
         wandb.run.summary["Average accuracy for experiment best"] = avg_accuracy
 
 
-def test_sequence_length():
-    # Test how generalization works for different lengths
-    splits = [24, 25, 26, 27, 28, 30, 32, 33, 36, 40, 48]
-
+def length_generalization(splits, x_label='Ground-truth action sequence length', plot_title='Sequence length'):
     results = defaultdict(list)
 
     for _ in range(n_runs):
-        input_lang = scan_dataset.Lang()
-        output_lang = scan_dataset.Lang()
-
-        train_dataset = scan_dataset.ScanDataset(
-            split=scan_dataset.ScanSplit.LENGTH_SPLIT,
-            input_lang=input_lang,
-            output_lang=output_lang,
-            train=True
-        )
-
-        encoder = models.EncoderRNN(train_dataset.input_lang.n_words, overall_best['HIDDEN_SIZE'], device,
-                                    overall_best['N_LAYERS'], overall_best['RNN_TYPE'], overall_best['DROPOUT']).to(
-            device)
-        decoder = models.DecoderRNN(train_dataset.output_lang.n_words, overall_best['HIDDEN_SIZE'],
-                                    overall_best['N_LAYERS'], overall_best['RNN_TYPE'], overall_best['DROPOUT'],
-                                    overall_best['ATTENTION']).to(device)
-
-        encoder, decoder = pipeline.train(train_dataset, encoder, decoder, n_iter, print_every=100, learning_rate=0.001,
-                                          device=device, log_wandb=log_wandb)
+        encoder = pickle.load(open('runs/overall_best_encoder_exp_2.sav', 'rb'))
+        decoder = pickle.load(open('runs/overall_best_decoder_exp_2.sav', 'rb'))
 
         # Evaluate on various lengths
         for split in splits:
@@ -151,84 +133,33 @@ def test_sequence_length():
         std_results[split] = np.std(result)
 
     # Plot bar chart
-    plt.bar(list(results.keys()), list(mean_results.values()), align='center', yerr=list(std_results.values()),
+    plt.bar(splits, list(mean_results.values()), align='center', yerr=list(std_results.values()),
             capsize=5)
-    plt.xlabel('Ground-truth action sequence length')
+    plt.xlabel(x_label)
+    plt.xticks()
     plt.ylabel('Accuracy on new commands (%)')
     plt.ylim((0., 1.))
 
     if log_wandb:
-        wandb.log({"Sequence length": plt})
+        wandb.log({plot_title: plt})
 
     plt.show()
 
     # Print results
     for split, result in results.items():
         print('Split: {}, Accuracy: {}'.format(split, sum(result) / len(result)))
+
+
+def test_sequence_length():
+    # Test how generalization works for different lengths
+    splits = [24, 25, 26, 27, 28, 30, 32, 33, 36, 40, 48]
+    length_generalization(splits)
 
 
 def test_command_length():
     # Test how generalization works for different command lengths
     splits = [4, 6, 7, 8, 9]
-
-    results = defaultdict(list)
-
-    for _ in range(n_runs):
-        input_lang = scan_dataset.Lang()
-        output_lang = scan_dataset.Lang()
-
-        train_dataset = scan_dataset.ScanDataset(
-            split=scan_dataset.ScanSplit.LENGTH_SPLIT,
-            input_lang=input_lang,
-            output_lang=output_lang,
-            train=True
-        )
-
-        encoder = models.EncoderRNN(train_dataset.input_lang.n_words, overall_best['HIDDEN_SIZE'], device,
-                                    overall_best['N_LAYERS'], overall_best['RNN_TYPE'], overall_best['DROPOUT']).to(
-            device)
-        decoder = models.DecoderRNN(train_dataset.output_lang.n_words, overall_best['HIDDEN_SIZE'],
-                                    overall_best['N_LAYERS'], overall_best['RNN_TYPE'], overall_best['DROPOUT'],
-                                    attention=overall_best['ATTENTION'], device=device).to(device)
-        encoder, decoder = pipeline.train(train_dataset, encoder, decoder, n_iter, print_every=100, learning_rate=0.001,
-                                          device=device)
-
-        # Evaluate on various lengths
-        for split in splits:
-            test_dataset = scan_dataset.ScanDataset(
-                split=scan_dataset.ScanSplit.LENGTH_SPLIT,
-                input_lang=input_lang,
-                output_lang=output_lang,
-                train=False
-            )
-
-            results[split].append(
-                pipeline.evaluate(test_dataset, encoder, decoder, max_length=MAX_LENGTH, verbose=False))
-
-    # Average results
-    mean_results = {}
-    for split, result in results.items():
-        mean_results[split] = sum(result) / len(result)
-
-    # Find standard deviation
-    std_results = {}
-    for split, result in results.items():
-        std_results[split] = np.std(result)
-
-    # Plot bar chart
-    plt.bar(list(results.keys()), list(mean_results.values()), align='center', yerr=list(std_results.values()),
-            capsize=5)
-    plt.xlabel('Command length')
-    plt.ylabel('Accuracy on new commands (%)')
-    plt.ylim((0., 1.))
-
-    if log_wandb:
-        wandb.log({"Command length": plt})
-    plt.show()
-
-    # Print results
-    for split, result in results.items():
-        print('Split: {}, Accuracy: {}'.format(split, sum(result) / len(result)))
+    length_generalization(splits, 'Command Length', 'Command Length')
 
 
 def main():
@@ -237,10 +168,10 @@ def main():
         wandb.login()
         wandb.init(project="experiment-2", entity="atnlp")
 
-    run_overall_best()
-    run_experiment_best()
-    # test_sequence_length()
-    # test_command_length()
+    # run_overall_best()
+    # run_experiment_best()
+    test_sequence_length()
+    test_command_length()
 
 
 if __name__ == '__main__':
