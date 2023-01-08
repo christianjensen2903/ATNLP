6d333bbc430780af0ce0f880994900d9e825ab30
christianjensen2903 <cmj290300@gmail.com>
2023-01-08 21:22:58 +0100

    ✨ Save and load model

--- Seq2SeqModel.py
+++ Seq2SeqModel.py
@@ -1,4 +1,6 @@
 import torch.nn as nn
+import wandb
+import torch
 
 # Abstract class for seq2seq models
 class Seq2SeqModel(nn.Module):
@@ -13,3 +15,22 @@ class Seq2SeqModel(nn.Module):
 
     def predict(self, input, max_length=100):
         raise NotImplementedError
+
+    def save(self, path: str, wandb_run = None, wandb_name = None):
+        torch.save(self.state_dict(), path)
+        if wandb_name and wandb_run:
+            artifact = wandb.Artifact(wandb_name, type='model')
+            artifact.add_file(path)
+            wandb_run.log_artifact(artifact)
+
+
+    def load(self, path: str, from_wandb, wandb_name = None):
+        if from_wandb and wandb_name:
+            artifact = wandb.use_artifact(wandb_name)
+            artifact_dir = artifact.download()
+            self.load_state_dict(torch.load(artifact_dir))
+        else:
+            self.load_state_dict(torch.load(path))
+
+
+        
