bbb7ea4a649af77673d6a1611ba4db85fa74a862
christianjensen2903 <cmj290300@gmail.com>
2023-01-13 14:28:46 +0100

    ♻️ Restructure experiment 3

---
+++ experiment-3_new.py
@@ -0,0 +1,76 @@
+import config
+import scan_dataset
+import RNNSeq2Seq
+import torch
+import wandb
+import numpy as np
+from tqdm import tqdm
+import Seq2SeqTrainer
+import Seq2SeqModel
+from typing import List, Dict, Tuple, Union, Optional
+import config
+from ExperimentBase import ExperimentBase
+
+
+class Experiment3(ExperimentBase):
+
+    def __init__(self,
+                 model: Seq2SeqModel.Seq2SeqModel,
+                 model_config: Seq2SeqModel.Seq2SeqModelConfig,
+                 train_args: Seq2SeqTrainer.Seq2SeqTrainingArguments,
+                 run_type: str,
+                 n_runs: int,
+                 split: scan_dataset.ScanSplit
+                  ):
+
+        self.split = split
+        super().__init__(model, model_config, train_args, run_type, n_runs, split=self.split)
+
+    def run(self):
+        self.train_models()
+        self.length_generalization(split=self.split, splits=['p1', 'p2', 'p4', 'p8', 'p16', 'p32', 'p64'], x_label='Percent of commands used for training', plot_title=f'training_pct_accuracy')
+
+
+
+def main():
+    train_args = config.paper_train_args
+
+    for model_type in ['overall_best', 'experiment_best']:
+        for split in [scan_dataset.ScanSplit.FEW_SHOT_SPLIT, scan_dataset.ScanSplit.ADD_PRIM_SPLIT]:
+            # Initialize wandb
+            if train_args.log_wandb:
+                wandb.init(
+                    project="experiment-3",
+                    entity="atnlp",
+                    config=train_args,
+                    reinit=True,
+                    tags=['experiment-3', 'overall_best', split])
+            
+
+            if model_type == 'overall_best':
+                model = RNNSeq2Seq.RNNSeq2Seq()
+                model_config = config.overall_best_config
+            elif model_type == 'experiment_best':
+                model = RNNSeq2Seq.RNNSeq2Seq()
+                model_config = RNNSeq2Seq.RNNSeq2SeqConfig(
+                    hidden_size=100,
+                    n_layers=1,
+                    dropout_p=0.5,
+                    attention=True,
+                    rnn_type="GRU",
+                    teacher_forcing_ratio=0.5,
+                )
+
+            Experiment3(
+                model=model,
+                model_config=model_config,
+                train_args=train_args,
+                run_type='overall_best',
+                n_runs=1,
+                split=split
+            ).run()
+
+
+
+if __name__ == '__main__':
+    main()
