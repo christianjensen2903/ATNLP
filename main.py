65396974b545a873f466cf588bdf2c9f2d96c1f0
christianjensen2903 <cmj290300@gmail.com>
2023-01-20 14:03:38 +0100

    ✨ Main file

---
+++ main.py
@@ -0,0 +1,13 @@
+import Experiment1
+import Experiment2
+import Experiment3
+
+
+def main():
+    Experiment1.main()
+    Experiment2.main()
+    Experiment3.main()
+
+
+if __name__ == "__main__":
+    main()
